adfb646534a880671af21525ccb478b64daf10a9
silvestre123456 <46685901@udaff.edu.pe>
2022-06-19 23:11:38 -0500

    barra de navegacion responsivo

--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Footer from '../Pages/Footer';
+import {useState} from 'react';
 import  '../styled-components/pagewrapper.css';
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faUser} from '@fortawesome/free-solid-svg-icons';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
 
 
 const PageWrapper = ({children}) => {
@@ -14,11 +16,19 @@ const PageWrapper = ({children}) => {
         <div className="navigation-titulo">
             <h1><span className="logo-title">T</span>romecito</h1>
         </div>
+        
+
         <nav className="navigation-nav">
+
+                    <input type="checkbox" id="check"/>
+                    <label htmlFor="check" className="checkLabel">
+                            <FontAwesomeIcon  icon={faBars} />
+                    </label>
+
                 <ul className="navigation-links">
                     <li><Link className="navigation-link" to="/home">Home</Link></li>
                     <li><Link className="navigation-link" to="/about" >About</Link></li>
-                    <li><span className="icono"><Link className="navigation-link" to="/login" ><FontAwesomeIcon  icon={faUser} /></Link></span>
+                    <li><span className="icono"><span className="navigation-link" to="/login" ><FontAwesomeIcon  icon={faUser} /></span></span>
                         <ul className="submenu">
                             <li><Link className="navigation-link" to="/login">Ingresar</Link></li>
                             <li><Link className="navigation-link" to="#">Registrarse</Link></li>
@@ -27,6 +37,8 @@ const PageWrapper = ({children}) => {
                     </li>
                 </ul>    
         </nav>
+
+        
     </header>
     {children}
 
